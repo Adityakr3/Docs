d718c6729089461d0d424b3ab13a6880382ed4f8
Aditya kumar <Adityakumar14022002@gmail.com>
2023-12-08 19:22:41 +0530

    dummy data and forground components

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,12 +1,51 @@
-import React from 'react'
-import Card from './Card'
+import React, { useRef } from "react";
+import Card from "./Card";
+import { motion } from "framer-motion"
 
 const Foreground = () => {
-  return (
-    <div className="fixed top-0 left-0 z-[3] w-full h-screen">
-      <Card/>
-    </div>
-  )
-}
 
-export default Foreground
+  const ref = useRef(null);
+
+  const data = [
+    {
+      title: "Lorem ipsum adipisicing elit. Placeat  iste quisquam!",
+      fileSize: ".5mb",
+      close: true,
+      tag: {
+        isOpen: false,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
+    {
+      title: "Lorem ipsum adipisicing elit. Placeat  iste quisquam!",
+      fileSize: ".5mb",
+      close: true,
+      tag: {
+        isOpen:true,
+        tagTitle: "Download Now",
+        tagColor: "sky",
+      },
+    },
+    {
+      title: "Lorem ipsum adipisicing elit. Placeat  iste quisquam!",
+      fileSize: ".5mb",
+      close: true,
+      tag: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
+  ];
+
+  return (
+    <div ref={ref} className="fixed top-0 left-0 z-[3] w-full h-screen flex gap-5">
+      {data?.map((item, idx) => (
+        <Card data={item} reference = {ref} />
+      ))}
+    </div>
+  );
+};
+
+export default Foreground;
